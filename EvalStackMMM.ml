f09a70ff21f82b6b6c964531a8d60be09e472b08
animalman33 <animalman3333@gmail.com>
2023-05-02 11:59:36 -0400

    Got rid of warnings

--- EvalStackMMM.ml
+++ EvalStackMMM.ml
@@ -4,22 +4,33 @@
 module EvalStack = struct
         class stack = object(self)
                 val mutable stack: string list = []
-                method push x = stack<-[x;]@stack
+
+                method push x = stack<-x::stack
+
                 method pop = match stack with
                 | [] -> None
-                | x::rest -> stack <- rest; Some(x)
+                | x::rest -> (stack <- rest); Some(x)
+
+                (* purely used for testing *)
                 method get_stack = stack
 
                 method peekOne = match stack with
                 | [] -> None
                 | x::rest -> Some(x)
+
                 method peekTwo = match stack with
                 | [] -> None
                 | x::[] -> None
                 | x::y::rest -> Some((x, y))
 
-                method replaceOne x = self#pop; self#push(x):
-                method replaceTwoWithOne x = self#pop; self#pop; self#push(x);
+                method replaceOne x = match stack with
+                  | [] -> None
+                  | y::rest -> (stack <- x::rest); Some(y)
+
+                method replaceTwoWithOne x = match stack with
+                  | [] -> None
+                  | _::[] -> None
+                  | y::z::rest -> (stack <- x::rest); Some((y, z))
         end;;
 
 end;;
